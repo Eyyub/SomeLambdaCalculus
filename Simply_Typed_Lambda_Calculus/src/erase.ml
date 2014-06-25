37b0388107f50833e700b1caf0d7732f01bf1257
Eyyüb SARI <eyyub.sari@epitech.eu>
2014-06-25 07:39:48 +0200

    Add erasure function.

---
+++ Simply_Typed_Lambda_Calculus/src/erase.ml
@@ -0,0 +1,13 @@
+let rec erase = function
+  | TypedTerm.True -> Term.True
+  | TypedTerm.False -> Term.False
+  | TypedTerm.Var (name, idx) -> Term.Var (name, idx)
+  | TypedTerm.Abs ((x, _), t) -> Term.Abs (x, erase t)
+  | TypedTerm.App (t1, t2) -> Term.App (erase t1, erase t2)
+  | TypedTerm.Assign (k, v) -> Term.Assign (k, erase v)
+
+let erase_all ty_e =
+  let rec aux acc = function
+    | [] -> List.rev acc
+    | x :: xs -> aux (erase x :: acc) xs
+  in aux [] ty_e
